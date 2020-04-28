cc32775b5f44a0c6602c123ba7b4c3dd5e41706f
LakshanPerera <lakshan.perera713@gmail.com>
2020-04-28 22:44:36 +0530

    added postgresql configs

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
-
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url("postgres://jbaiigtqsownpu:1f6aaca80958fb291cdbb39b2a8df0ed1863bacbb0d49822f737d4e9e64526cb@ec2-18-235-20-228.compute-1.amazonaws.com:5432/d2a0n9ffa6g0ds");
 return [
 
     /*
@@ -63,19 +63,31 @@ return [
             ]) : [],
         ],
 
+//        'pgsql' => [
+//            'driver' => 'pgsql',
+//            'url' => env('DATABASE_URL'),
+//            'host' => env('DB_HOST', '127.0.0.1'),
+//            'port' => env('DB_PORT', '5432'),
+//            'database' => env('DB_DATABASE', 'forge'),
+//            'username' => env('DB_USERNAME', 'forge'),
+//            'password' => env('DB_PASSWORD', ''),
+//            'charset' => 'utf8',
+//            'prefix' => '',
+//            'prefix_indexes' => true,
+//            'schema' => 'public',
+//            'sslmode' => 'prefer',
+//        ],
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
