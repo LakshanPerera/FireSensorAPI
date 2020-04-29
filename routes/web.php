f4a18d24e3300b76179c1f7961f8ccd13e3269a9
LakshanPerera <lakshan.perera713@gmail.com>
2020-04-29 11:59:52 +0530

    login path updated

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::post('/login', 'AuthLoginController@login')->name('api-login-route');
+//Route::post('/login', 'AuthLoginController@login')->name('api-login-route');
 Route::get('/', function () {
     return view('welcome');
 });
